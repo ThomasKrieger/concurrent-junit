7bf8cccdf3a0d1ebe0379b5c839f28e632d6755a
ThomasKrieger <thomas.krieger@vmlens.com>
2017-11-20 13:25:59 +0100

    new java doc

--- concurrent-junit/src/main/java/com/anarsoft/vmlens/concurrent/junit/TestUtil.java
+++ concurrent-junit/src/main/java/com/anarsoft/vmlens/concurrent/junit/TestUtil.java
@@ -3,8 +3,42 @@ package com.anarsoft.vmlens.concurrent.junit;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 
+ * static utility methods for testing methods concurrently.
+ * 
+ * @author thomas
+ *
+ */
+
+
 public class TestUtil {
 
+	
+	/**
+	 * 
+	 * Runs the runnables with threadCount threads in parallel.
+	 * Example usage:
+	 * <pre>
+	 *     	TestUtil.runMultithreaded( new Runnable() {
+	 *		public void run() {
+	 *			try{
+	 *				shouldFindAllPetTypes();
+	 *			}
+	 *			catch(Exception e)
+	 *			{
+	 *				e.printStackTrace();
+	 *			}
+	 *		}
+     *	}
+     *	, 5);
+	 * </pre>
+	 * 
+	 * 
+	 * @param runnable
+	 * @param threadCount
+	 * @throws InterruptedException
+	 */
 	
 	
 	public static void runMultithreaded(Runnable  runnable, int threadCount) throws InterruptedException
--- concurrent-junit/src/main/java/com/anarsoft/vmlens/concurrent/junit/ThreadCount.java
+++ concurrent-junit/src/main/java/com/anarsoft/vmlens/concurrent/junit/ThreadCount.java
@@ -3,7 +3,21 @@ package com.anarsoft.vmlens.concurrent.junit;
 import java.lang.annotation.Retention;
 import static java.lang.annotation.RetentionPolicy.*;
 
+/**
+ * 
+ * Annotation for a test method. The annotated test method will be run by ThreadCount threads. 
+ * 
+ * @author thomas
+ *
+ */
+
+
 @Retention(value=RUNTIME)
 public @interface ThreadCount {
-     int value();
+     
+	/** 
+     * the number of threads to use
+     */
+	int value();
+     
 }
